f320792676b4cc301069daa36c9be8839cfd6e13
Bruno Manganoti <brunomanganoti@gmail.com>
2026-05-18 13:19:12 -0300

    inicializando repositório

---
+++ scripts/data_clean.R
@@ -0,0 +1,9 @@
+# Script para tratamento dos dados
+# Arquivo: serieA-season-2425.csv
+
+# Carregando lib para manipulação de dados
+library(tidyverse)
+
+data_2425 <- read.csv("data/raw/serieA-season-2425.csv")
+
+View(data_2425)
